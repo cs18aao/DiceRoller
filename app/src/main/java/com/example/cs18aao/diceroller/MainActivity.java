d2d5e5ea2979edde9fc74da5e80a918b48314eae
akwasi <1823855@brunel.ac.uk>
2019-10-23 19:41:05 +0100

    part 2 and 3 done

--- app/src/main/java/com/example/cs18aao/diceroller/MainActivity.java
+++ app/src/main/java/com/example/cs18aao/diceroller/MainActivity.java
@@ -8,11 +8,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText userInput;
+    private int counter = 0;
+    private TextView points;
+    private TextView counting;
 
 
 
@@ -22,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textView = (TextView) findViewById(R.id.textView);
+        userInput = (EditText) findViewById(R.id.userInput);
+        points = (TextView) findViewById(R.id.points);
+        counting = (TextView) findViewById(R.id.counting);
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -60,12 +77,32 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = (TextView) this.findViewById(R.id.textView);
 
         Random r = new Random();
-        int number = r.nextInt((6)+1);
+        int number = r.nextInt(6);
 
         tv.setText(Integer.toString(number));
 
 
-
-
+        int  num = Integer.parseInt(userInput.getText().toString());
+        if (num < 1 || num > 6)
+        {
+            Toast.makeText(this, "Invalid input, the number must be 1 - 6", Toast.LENGTH_SHORT).show();
+        }
+        else if (num == number)
+        {
+            Toast.makeText(this, "congratulations, you guessed right!", Toast.LENGTH_SHORT).show();
+            counter = counter + 1;
+            points.setText(Integer.toString(counter));
+        }
     }
+
 }
+
+
+
+
+
+
+
+
+
+
